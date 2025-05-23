ac7546c9fd21b0533c370844646a5a2aaf8a1bf8
Gabriele <gabrieletuozzo@gmail.com>
2025-05-23 15:30:00 +0000

    fix: add missing './' in unzip command for correct file path

--- reproduce_results.sh
+++ reproduce_results.sh
@@ -1,7 +1,7 @@
 apt update
 apt install zip
 apt install unzip
-unzip data/quality_data/kghb_output/quality_data.zip -d data/quality_data/kghb_output/
+unzip ./data/quality_data/kghb_output/quality_data.zip -d ./data/quality_data/kghb_output/
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
