834f85aaadca26ea5188efa3079d744e5a0a203b
Gabriele <gabrieletuozzo@gmail.com>
2025-05-23 13:44:35 +0000

    added script to reproduce the results presented in the article

---
+++ reproduce_results.sh
@@ -0,0 +1,9 @@
+apt update
+apt install zip
+apt install unzip
+unzip data/quality_data/kghb_output/quality_data.zip -d data/quality_data/kghb_output/
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+cd src
+python3 main.py
